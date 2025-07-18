0633e5cce53ea2940e8dc62f77d74b0456564b45
s3basj <sebasjaramillo0@hotmail.com>
2025-07-18 12:11:43 -0500

    Configure deployment with Vite and GitHub Pages: added vite.config.js, gh-pages scripts, and linked new project directory

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/final-react-conference-planner-ibm",
   plugins: [react()],
-})
+});

63f76a1c62d996cadfe553680c8b216410076b41
s3basj <sebasjaramillo0@hotmail.com>
2025-08-07 16:35:27 -0500

    feat: implementar los reducers del carrito en CartSlice

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,20 +1,27 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 export const CartSlice = createSlice({
-  name: 'cart',
+  name: "cart",
   initialState: {
     items: [], // Initialize items as an empty array
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const existingItem = state.items.find(
+        (item) => item.name === action.payload.name
+      );
+
+      if (existingItem) {
+        existingItem.quantity++;
+      } else {
+        state.items.push({ ...action.payload, quantity: 1 });
+      }
     },
     removeItem: (state, action) => {
+      const itemName = action.payload;
+      state.items = state.items.filter((item) => item.name !== itemName);
     },
-    updateQuantity: (state, action) => {
-
-    
-    },
+    updateQuantity: (state, action) => {},
   },
 });
 

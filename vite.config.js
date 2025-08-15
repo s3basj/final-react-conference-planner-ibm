b37fa807dd3814261a64b14a1e97fa91cb6accb8
s3basj <sebasjaramillo0@hotmail.com>
2025-08-15 14:00:00 -0500

    fix: cambiar nombre del proyecto al real

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/final-react-conference-planner-ibm/",
+  base: "/final-react-e-plant-shop-ibm/",
   plugins: [react()],
 });
